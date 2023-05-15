95bcb4b9e44f298d7247d34dd6442435810c10ca
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2023-05-15 15:41:11 -0300

    feat: using react-hook-form in SignUpPage with error validation display

--- src/Pages/SignUpPage/SignUp-Page.js
+++ src/Pages/SignUpPage/SignUp-Page.js
@@ -1,61 +1,128 @@
-import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import logo from "../../Assets/imgs/Group8.png"
-import axios from "axios"
-import { LoginForm } from "../../Constants/StyledComponents"
-import { ThreeDots } from "react-loader-spinner"
-import { PageContainer } from "../SignInPage/SignInStyles"
-import { url } from "../../Constants/urls"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../Assets/imgs/Group8.png";
+import axios from "axios";
+import { LoginForm } from "../../Constants/StyledComponents";
+import { ThreeDots } from "react-loader-spinner";
+import { PageContainer } from "../SignInPage/SignInStyles";
+import { url } from "../../Constants/urls";
+import { useForm } from "react-hook-form";
 
-export default function SignUpPage () {
-    const [disable, setDisable] = useState(false)
-    const navigate = useNavigate();
-    const [form ,setForm] = useState({ email: "", name: "", image: "", password: "" }); 
+export default function SignUpPage() {
+  const [disable, setDisable] = useState(false);
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      name: "",
+      image: "",
+      password: "",
+    },
+  });
 
-    function register (e) {
-        setDisable(true)
-        e.preventDefault();
-        axios.post(`${url}/auth/sign-up`, form)
-        .then(res => {navigate ("/")})
-        .catch(err => {
-            alert(err.response.data.message);
-            setDisable(false);
-        })
-    }
+  const onSubmit = (formData) => {
+    setDisable(true);
+    axios
+      .post(`${url}/auth/sign-up`, formData)
+      .then((res) => {
+        navigate("/");
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+        setDisable(false);
+      });
+  };
 
+  return (
+    <PageContainer>
+      <img src={logo} alt="Trackt Logo" />
 
-    function handleForm (e) {
-        setForm({
-            ...form,
-            [e.currentTarget.name]: e.currentTarget.value,
-          });
-    }
+      <LoginForm onSubmit={handleSubmit(onSubmit)}>
+        <input
+          disabled={disable}
+          data-identifier="input-email"
+          {...register("email", {
+            required: true,
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Insira um formato de e-mail válido",
+            },
+          })}
+          type="email"
+          name="email"
+          placeholder="email"
+        ></input>
+        {errors.email?.type === "required" && (
+          <p role="alert">Forneça um e-mail válido</p>
+        )}
+        {errors.email?.message && <p role="alert">{errors.email?.message}</p>}
 
-    return (
-        <PageContainer>
-        <img src={logo} alt="Trackt Logo"/>
+        <input
+          disabled={disable}
+          data-identifier="input-password"
+          {...register("password", { required: true, minLength: {
+            value: 6,
+            message: 'A senha deve conter ao menos 6 caracteres'
+          } })}
+          type="password"
+          name="password"
+          placeholder="senha"
+        ></input>
+        {errors.password?.type === "required" && (
+          <p role="alert">Forneça uma senha</p>
+        )}
+        {errors.password?.message && <p role="alert">{errors.password?.message}</p>}
+        <input
+          disabled={disable}
+          data-identifier="input-name"
+          {...register("name", { required: true })}
+          type="text"
+          name="name"
+          placeholder="nome"
+        ></input>
+        {errors.name?.type === "required" && <p role="alert">Forneça nome</p>}
+        <input
+          disabled={disable}
+          data-identifier="input-photo"
+          {...register("image", { required: true,
+        pattern: {
+            value: /^((http|https):\/\/)/,
+            message: 'Insira um formato válido de URL (https://...)'
+        } })}
+          type="text"
+          name="image"
+          placeholder="coloque a URL de uma imagem"
+        ></input>
+        {errors.image?.type === "required" && (
+          <p role="alert">Forneça a url da imagem</p>
+        )}
+        {errors.image?.message && <p role="alert">{errors.image?.message}</p>}
 
-        <LoginForm onSubmit={register}>
-            <input required disabled={disable} data-identifier="input-email" onChange={handleForm} type="email" name="email" placeholder="email"></input>
-            <input required disabled={disable} data-identifier="input-password" onChange={handleForm} type="password" name="password" placeholder="senha"></input>
-            <input required disabled={disable} data-identifier="input-name" onChange={handleForm} type="text" name="name" placeholder="nome"></input>
-            <input required disabled={disable} data-identifier="input-photo" onChange={handleForm} type="text" name="image" placeholder="coloque a URL de uma imagem"></input>
-            <button type="submit" >{disable ? 
-            <ThreeDots 
-            height="30" 
-            width="80" 
-            radius="9"
-            color="white" 
-            ariaLabel="three-dots-loading"
-            wrapperStyle={{}}
-            wrapperClassName=""
-            visible={true}
-             />
-            : "Cadastrar"}</button>
-        </LoginForm>
+        <button type="submit">
+          {disable ? (
+            <ThreeDots
+              height="30"
+              width="80"
+              radius="9"
+              color="white"
+              ariaLabel="three-dots-loading"
+              wrapperStyle={{}}
+              wrapperClassName=""
+              visible={true}
+            />
+          ) : (
+            "Cadastrar"
+          )}
+        </button>
+      </LoginForm>
 
-        <Link data-identifier="back-to-login-action" to="/"><p>Já tem uma conta? Faça login!</p></Link>
-        
+      <Link data-identifier="back-to-login-action" to="/">
+        <p>Já tem uma conta? Faça login!</p>
+      </Link>
     </PageContainer>
-    )
+  );
 }
